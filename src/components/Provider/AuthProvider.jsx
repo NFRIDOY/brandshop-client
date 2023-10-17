bd992bc03b5d624c4db221b820c23b3aa9a1d0f3
NF RIDOY <nfridoy@gmail.com>
2023-10-18 01:43:29 +0600

    Feat: Firebase email auth, google auth init setup

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,13 +1,26 @@
 import { createContext, useState } from "react"
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
 
 export const AuthContext = createContext(null)
 
 export default function AuthProvider({ children }) {
 
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+    
+    const createUserByEmail = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+    
     const [count, setCount] = useState(0)
     const test = 10
-    const infoValue = { test, count, setCount };
+    const infoValue = { test, count, setCount, createUserByEmail, signInGoogle };
     return (
         <AuthContext.Provider value={infoValue}>
             {children}
