ce678280d3bc202ae49a86cd71e5515049f46e6b
NF RIDOY <nfridoy@gmail.com>
2023-10-18 17:27:37 +0600

    brand cards

---
+++ src/components/BrandNameCard/BrandNameCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+
+export default function BrandNameCard({ brand }) {
+    const { _id, brandId, brandImage, brandName } = brand
+
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+                <Link to={''}>
+                    <img src={brandImage} className="object-cover h-80" alt="Shoes" />
+                </Link>
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title justify-center text-3xl font-bold">{brandName}</h2>
+                {/* <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div> */}
+            </div>
+        </div>
+    )
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Banner from "../Banner/Banner";
 import { AuthContext } from "../Provider/AuthProvider";
+import { data } from "autoprefixer";
+import BrandNameCard from "../BrandNameCard/BrandNameCard";
 
 
 export default function Home() {
     const { test, currentUser, setCurrentUser, createUserByEmail, signInGoogle } = useContext(AuthContext)
+    const [brandNames, setBrandNames] = useState([])
+    fetch("http://localhost:5000/brandNames")
+        .then(res => res.json())
+        .then(data => {
+            setBrandNames(data)
+            console.log(data)
+        })
     return (
         <div>
             <Banner></Banner>
-            Home {test}
-            
+            {/* Home {test} */}
+            <div className="max-w-7xl mx-auto p-8 text-center ">
+                <h1 className="my-8 text-8xl font-bold">Our Brands</h1>
+                <div className="grid grid-cols-3 gap-y-16">
+                    {
+                        brandNames.map(brand => <BrandNameCard key={brand._id} brand={brand} ></BrandNameCard>)
+                    }
+                </div>
+            </div>
+
         </div>
     )
 }
