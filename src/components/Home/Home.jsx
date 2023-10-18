a01f30d21c6f95005c42a9e3f9f39234f8b2521a
NF RIDOY <nfridoy@gmail.com>
2023-10-18 21:13:49 +0600

    Fix: on Home map is not a function

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,11 +21,12 @@ export default function Home() {
         <div>
             <Banner></Banner>
             {/* Home {test} */}
+            {loading}
             <div className="max-w-7xl mx-auto p-8 text-center ">
                 <h1 className="my-8 text-8xl font-bold">Our Brands</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-y-16">
                     {
-                        brandNames.map(brand => <BrandNameCard key={brand._id} brand={brand} ></BrandNameCard>)
+                        brandNames?.map(brand => <BrandNameCard key={brand._id} brand={brand} ></BrandNameCard>)
                     }
                 </div>
             </div>
